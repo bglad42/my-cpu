0840b71f88b82925b00ab1869e8abf65f8960328
Brayden Gladman <130877719+bglad42@users.noreply.github.com>
2025-10-27 20:39:53 -0700

    Moved ALU files to dedicated folder

--- mux4_1.sv
+++
@@ -1,9 +0,0 @@
-`timescale 1ps/1ps
-module mux4_1(i0, i1, i2, i3, sel0, sel1, out);
-	input logic i0, i1, i2, i3, sel0, sel1;
-	output logic out;
-	logic s0, s1;
-	mux2_1 r0(.i0(i0), .i1(i1), .sel(sel0), .out(s0));
-	mux2_1 r1(.i0(i2), .i1(i3), .sel(sel0), .out(s1));
-	mux2_1 r2(.i0(s0), .i1(s1), .sel(sel1), .out(out));
-endmodule 
