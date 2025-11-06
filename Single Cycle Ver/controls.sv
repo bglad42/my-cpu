83954756473667cf7de6b30290d2b3f87387e8ff
Brayden Gladman <130877719+bglad42@users.noreply.github.com>
2025-11-05 23:40:49 -0800

    zero extend + controls modification

--- Single Cycle Ver/controls.sv
+++ Single Cycle Ver/controls.sv
@@ -4,17 +4,22 @@ module controls (Reg2Loc, UncondBr, BrTaken, RegWrite, MemWrite, ALUOp, ALUSrc, 
 	input logic zero;
 	
 	enum logic [10:0] {
-		ADD = 11'b10001011000;
-		SUB = 11'b11001011000;
-		LDUR = 11'b11111000010;
-		STUR = 11'b11111000000;
-		B = 11'b000101xxxxx;
-		CBZ = 11'b10110100xxx;
+		ADDS 	= 11'b10101011000,
+		ADDI	= 11'b10010001000,
+		AND 	= 11'b10001010000,
+		SUBS 	= 11'b11001011000,
+		LDUR	= 11'b11111000010,
+		STUR 	= 11'b11111000000,
+		B 		= 11'b000101XXXXX,
+		CBZ 	= 11'b10110100XXX,
+		BLT 	= 11'b01010100XXX,
+		EOR 	= 11'b11001010000,
+		LSR	= 11'b11010011011
 	} OpCodes;
 	
 	always_comb begin
 		case (instr[31:21]) begin
-			ADD: begin
+			ADDS: begin
 				Reg2Loc 	= 1'b1;
 				ALUSrc 	= 1'b0;
 				MemToReg = 1'b0;
@@ -25,7 +30,18 @@ module controls (Reg2Loc, UncondBr, BrTaken, RegWrite, MemWrite, ALUOp, ALUSrc, 
 				ALUOp 	= 3'b010;
 			end
 			
-			SUB: begin
+			ADDI: begin
+				Reg2Loc 	= 1'b1;
+				ALUSrc 	= 1'b0;
+				MemToReg = 1'b0;
+				RegWrite = 1'b1;
+				MemWrite = 1'b0;
+				BrTaken	= 1'b0;
+				UncondBr = 1'bX;
+				ALUOp 	= 3'b010;
+			end
+			
+			SUBS: begin
 				Reg2Loc 	= 1'b1;
 				ALUSrc 	= 1'b0;
 				MemToReg = 1'b0;
--- Single Cycle Ver/singleCycle.sv
+++ Single Cycle Ver/singleCycle.sv
@@ -8,6 +8,7 @@ module singleCycle (clk, reset);
 	wire [63:0] pc, newpc;
 	logic UncondBr, BrTaken, Reg2Loc, RegWrite, ALUSrc, MemWrite, MemToReg; // controls
 	logic [2:0] ALUOp; // more control
+	logic [3:0] ALUSrc; // more
 	
 	// flags
 	
@@ -23,7 +24,7 @@ module singleCycle (clk, reset);
 	controls broisthethinker (.Reg2Loc(Reg2Loc), .UncondBr(UncondBr), .BrTaken(BrTaken), .RegWrite(RegWrite),
 									  .MemWrite(MemWrite), .ALUOp(ALUOp), .ALUSrc(ALUSrc), .MemToReg(MemToReg), .instr(instr)); // gen control signals
 									  
-	/* BIG PROGRAM COUNTER BLOCK start*/
+	/* BIG IFETCH BLOCK start*/
 	
 	programCounter current (.q(pc), .d(newpc), .clk(clk), .reset(reset)); // out to pc, in from newpc
 	
@@ -38,7 +39,7 @@ module singleCycle (clk, reset);
 	adder_64 PCUpdate (.out(nobranch), .A(pc), .B(64'd4)); // update pc on no branch
 	mux_64_2_1 branchOrNot (.out(newpc), .A(nobranch), .B(branchPC), .sel(BrTaken)); // select new pc address 
 	
-	/* BIG PROGRAM COUNTER BLOCK END */
+	/* BIG IFETCH BLOCK END */
 	
 	/* ////////////////////////////////////////////////////////////////////////////// */
 	
@@ -62,10 +63,24 @@ module singleCycle (clk, reset);
 	regfile reggie (.ReadData1(Da), .ReadData2(Db), .WriteData(Dw), .ReadRegister1(Ain),
 						ReadRegister2(Rn), .WriteRegister(Rd), .RegWrite(RegWrite), .clk(clk), .reset(reset)); // figure out wite data and put here
 						
-	wire [63:0] ALUResult, DAddr9, ALUBin;
+	wire [63:0] ALUResult, DAddr, ALUBin, Imm;
 	
-	sign_extend #(.WIDTH(9)) Addr (.out(DAddr9), .in(instr[20:12])); // Prepare address for LDUR/STUR
-	mux_64_2_1 ALUBinputSrc (.out(ALUBin), .A(Db), .B(DAddr9), .sel(ALUSrc)); // select second ALU operand
+	sign_extend #(.WIDTH(9)) Addr (.out(DAddr), .in(instr[20:12])); // Prepare address for LDUR/STUR
+	
+	// mux_64_2_1 ALUBinputSrc (.out(ALUBin), .A(Db), .B(DAddr), .sel(ALUSrc)); // replace for imm12
+	
+	zero_extend #(.WIDTH(12)) Imm12 (.out(Imm), .in(instr[21:10]));
+	
+	generate begin
+		for (i = 0; i < 64; i++): each4Mux
+			wire [3:0] temp;
+			assign temp[0] = Db[i];
+			assign temp[1] = DAddr[i];
+			assign temp[2] = Imm[i];
+			assign temp[3] = 1'bX;
+			mux_4_1(.out(ALUBin), .in(temp), .sel(ALUSrc));
+		end
+	endgenerate
 	
 	wire ALUz, ALUo, ALUc, ALUn;
 	
@@ -74,7 +89,7 @@ module singleCycle (clk, reset);
 	wire [63:0] dataOut;
 	datamem dataMemory (.address(ALUResult), .write_enable(MemWrite), .read_enable(1'b1) /*temp*/, // read up on xfer size, read enable reqs
 								.write_data(Db), .clk(clk), .xfer_size(1'b1), .read_data(dataOut));
-								
+								*
 	mux_64_2_1 toReg (.out(Dw), .A(ALUResult), .B(dataOut), .sel(MemToReg));
 
 endmodule
